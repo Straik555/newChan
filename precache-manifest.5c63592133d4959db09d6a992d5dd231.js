001fb759f5e354f23071d05e180e9cb077161af4
Straik555 <54748889+Straik555@users.noreply.github.com>
2021-06-11 14:50:49 +0300

    first

---
+++ precache-manifest.5c63592133d4959db09d6a992d5dd231.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1eff8c0dd6208408608e7a40a080e0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fac0b8f3533501f2b656",
+    "url": "/static/css/main.fe68ee06.chunk.css"
+  },
+  {
+    "revision": "2202dea19dfe669b1b68",
+    "url": "/static/js/2.d6ce9de4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d6ce9de4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fac0b8f3533501f2b656",
+    "url": "/static/js/main.d3dff425.chunk.js"
+  },
+  {
+    "revision": "dc115263a54ad89ca4f7",
+    "url": "/static/js/runtime-main.2e3ad8f3.js"
+  },
+  {
+    "revision": "95eef8bb6afe10255f9d2eff806d7e2d",
+    "url": "/static/media/react.95eef8bb.svg"
+  }
+]);
